6927eb9f25a2aa8d753bd1e24c0770c8ba1d4e51
Tapir79 <saara.m.pakarinen@gmail.com>
2019-10-01 10:22:36 +0300

    remove unused state

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,11 +4,7 @@ import Container from './components/Container';
 
 /* Main Component of our weather-application */
 class App extends Component {
-  state = {
-    mapCoords: [-40.0, 40.0], // coordinates default to Atlantic
-  };
-
-
+ 
   /* React's render method: defines what the component should render on the UI */
   render() {
     return (
