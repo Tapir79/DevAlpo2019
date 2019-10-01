694303403603b1d71f564cbb4723b13effca95f1
Tapir79 <saara.m.pakarinen@gmail.com>
2019-10-01 13:27:58 +0300

    change if-else to ternary

--- weather-app/src/utils/utilFunctions.js
+++ weather-app/src/utils/utilFunctions.js
@@ -1,14 +1,12 @@
 // randomly generates the weather in celsius degrees between -30 and 30
 export const randomWeather = () => {
     const isMinus = Math.floor((Math.random() * 10) + 1) > 5 ? 1 : -1 ;
-        const randomWeather = (Math.floor((Math.random() * 30) + 1)) ;
-        return randomWeather  * isMinus;    
+    const randomWeather = (Math.floor((Math.random() * 30) + 1)) ;
+    return randomWeather  * isMinus;    
 }
 
-export const getMapStyle = (weather) => {
-    if(weather > 0){
-       return 'mapbox://styles/mapbox/streets-v9'
-    } else {
-       return 'mapbox://styles/mapbox/light-v10'
-    }
+// returns a summer like mapstyle if tempreature is above 0
+// returns a winter like mapstyle if temperature is below or equal to 0
+export const getMapStyle = (temperature) => {
+  return temperature > 0 ? 'mapbox://styles/mapbox/streets-v9' : 'mapbox://styles/mapbox/light-v10'
 }
