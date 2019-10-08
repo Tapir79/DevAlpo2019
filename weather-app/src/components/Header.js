067da4367c3aebc09623bf950f478385ff1acd0b
Tapir79 <saara.m.pakarinen@gmail.com>
2019-10-08 08:31:28 +0300

    add weatherlogic function and header texts

--- weather-app/src/components/Header.js
+++ weather-app/src/components/Header.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import TypoGraphy from '@material-ui/core/Typography';
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import TypoGraphy from "@material-ui/core/Typography";
 
+const shouldGoForCoffee = weather => {
+  return weather < -10 || weather > 25
+    ? "Jää kotiin kahville"
+    : "No eiköhän mennä kahvilaan!";
+};
 
 class Header extends Component {
-
-    render() {
-        return <AppBar color='primary' position='static'>
-            <Toolbar>
-                <TypoGraphy variant='title'
-                    color='inherit'>                      
-                   <h1>Sää Tampereella on: {this.props.weather}</h1> 
-                </TypoGraphy>
-            </Toolbar>
-
-        </AppBar>
-    }
+  render() {
+    return (
+      <AppBar color="primary" position="static">
+        <Toolbar>
+          <TypoGraphy variant="h6" color="inherit">
+            <h1>Sää Tampereella on: {this.props.weather} astetta</h1>
+            <h2> {shouldGoForCoffee(this.props.weather)} </h2>
+          </TypoGraphy>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
 export default Header;
