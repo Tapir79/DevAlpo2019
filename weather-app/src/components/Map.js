33e3a28766cd958917c4231f9a76ed36e338c96a
Tapir79 <saara.m.pakarinen@gmail.com>
2019-10-08 08:10:52 +0300

    example solution 1

--- weather-app/src/components/Map.js
+++ weather-app/src/components/Map.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
-import { accessToken } from '../mapbox.config.js';
+import React, { Component } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { accessToken } from "../mapbox.config.js";
 //import * as cafeData from '../data/cafe.json';
 //import * as cafeImage from '../images/cafe.png';
-import { ApiController } from '../controllers/ApiController';
+import { ApiController } from "../controllers/ApiController";
 
 /* Component wrapping the mapbox map */
 class Map extends Component {
-
   state = {
     viewport: {
       width: window.innerWidth,
@@ -33,29 +32,38 @@ class Map extends Component {
                 latitude: treCenter[1],
                 longitude: treCenter[0]
               }
-            })
+            });
           }
         });
       }
     });
-  };
+  }
 
   render() {
     /* Set configurations to ReactMapGL's viewport */
-    const viewport = { ...this.state.viewport }
+    const viewport = { ...this.state.viewport };
     return (
       <div>
         <ReactMapGL
           mapStyle={this.props.mapStyleUrl}
           {...viewport}
-          onViewportChange={(viewport) => this.setState({ viewport })}
-          mapboxApiAccessToken={accessToken}>
-          {/* TODO 1. add a Marker and an icon*/}
+          onViewportChange={viewport => this.setState({ viewport })}
+          mapboxApiAccessToken={accessToken}
+        >
+          <Marker
+            key={0}
+            latitude={61.4990956}
+            longitude={23.7571622}
+            offsetLeft={-15}
+            offsetTop={-15}
+          >
+            <div>Hello Marker World</div>
+          </Marker>
           {/* TODO 2. add multiple markers from cafeData */}
         </ReactMapGL>
       </div>
     );
   }
-};
+}
 
 export default Map;
